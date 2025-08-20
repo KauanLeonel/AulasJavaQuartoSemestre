491de779a442384189eaa2c339579fc7472f498d
KauanLeonel <kauan.br.char@gmail.com>
2025-08-19 21:46:15 -0300

    Aula 19/08 - Projeto strategy

---
+++ ExercicioBanco/src/br/com/Banco/service/Comparator.java
@@ -0,0 +1,15 @@
+package br.com.Banco.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.Banco.model.ContaCorrente;
+
+public class Comparator {
+    
+    public static void saldoDecrescente(List<ContaCorrente> contas){
+        Comparator<ContaCorrente> porSalário = (c1, c2) -> Double.compare(c1.getSaldo(), c2.getSaldo())
+        List<ContaCorrente> ordenados = new ArrayList<>(ContaCorrente);
+        ordenados.sort(porSalário);
+    }
+}
